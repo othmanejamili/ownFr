b40490f3d5cf92be029158cad8aec476d17f5817
OthFR <othmanejamili19@gmail.com>
2026-04-24 18:24:02 +0100

    fix URL

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 
 const AuthContext = createContext();
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser]                       = useState(null);
