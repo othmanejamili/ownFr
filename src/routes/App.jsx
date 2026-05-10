2b876eb89cb04c0e834d96329af063cc7588347d
OthFR <othmanejamili19@gmail.com>
2026-05-10 17:59:29 +0100

    fix problem

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,13 +3,15 @@ import Register           from '../pages/auth/RegisterPage';
 import Login              from '../pages/auth/LoginPage';
 import HomePage           from '../pages/Home/HomePage';
 import AdminDashboard     from '../pages/Dashboard/Admin/AdminDashboard';
+import OwnerDashboard from '../pages/Dashboard/Owner/OwnerDashboard';
 import LogoutButton       from '../pages/auth/Logout';
 import ProtectedRoute     from '../components/ProtectedRoute';
 import ForgotPassword from '../pages/auth/ForgotPassword';
 import Pricing from '../pages/Home/Pricing';
 import SchoolsPage from '../pages/Home/SchoolsPage';
 import OwnerDashboard from '../pages/Dashboard/Owner/OwnerDashboard';
-
+import Blog from '../pages/Home/Blogs';
+import Docs from '../pages/Home/Docs';
 function App() {
   return (
     <Router>
@@ -19,6 +21,8 @@ function App() {
         <Route path="/"         element={<HomePage />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path='/schools'  element={<SchoolsPage />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/docs' element={<Docs />} />
         <Route path="/login"    element={<Login />} />
         <Route path="/logout"   element={<LogoutButton />} />
         <Route path="/register" element={<Register />} />
